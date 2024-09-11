cf313aff6aeb6f335ca08d39c00639c7d4bfa4da
Youssef E. Saber <emadyoussef247@gmail.com>
2024-09-11 20:24:37 +0300

    Add files via upload

---
+++ Assignment_1/DB/Models/brand.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+const { Schema, model } = mongoose;
+
+
+const brandSchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        slug: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        createdBy: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: false,
+        },
+        logo: {
+            secure_url: {
+                type: String,
+                required: true,
+            },
+            public_id: {
+                type: String,
+                required: true,
+                unique: true,
+            },
+        },
+        customId: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        categoryId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Category",
+            required: true,
+        },
+    subCategoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+        ref: "subCategory",
+    required: true,
+}
+    },
+    { timestamps: true }
+);
+
+export const brand =
+    mongoose.models.brand|| model("brand", brandSchema);
+
+
---
+++ Assignment_1/src/Middlewares/finders.middleware.js
@@ -0,0 +1,43 @@
+import { ErrorClass } from "../Utils/index.js";
+export const getDocumentByName = (model) => {
+  return async (req, res, next) => {
+    const { name } = req.body;
+    if (name) {
+      const document = await model.findOne({ name });
+      if (document) {
+        return next(
+          new ErrorClass(
+            " this name already exists",
+            400,
+            " this name already exists"
+          )
+        );
+      }
+    }
+    next();
+  };
+};
+
+
+export const checkIfIdsExit = (model) => {
+  return async (req, res, next) => {
+
+    //Ids check
+    const { category, subCategory, brandId } = req.query
+    const Document = await model.findOne({
+      _id: brandId,
+      categoryId: category,
+      subCategoryId: subCategory
+    }).populate([
+      { path: "categoryId", select: "customId" },
+      { path: "subCategoryId", select: "customId" }
+    ])
+
+    if (!Document) {
+      return next(ErrorClass("Brand Not found", 404))
+    }
+    req.document=Document
+    next()
+
+  }
+}
