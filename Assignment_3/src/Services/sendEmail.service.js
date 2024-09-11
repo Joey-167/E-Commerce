57be2451c5e31918f1c86ed8e11e193af5b3a681
Youssef E. Saber <emadyoussef247@gmail.com>
2024-09-11 20:59:17 +0300

    Update sendEmail.service.js

--- Assignment_3/src/Services/sendEmail.service.js
+++ Assignment_3/src/Services/sendEmail.service.js
@@ -7,17 +7,13 @@ export const sendEmailService = async ({
     attachments = [],
 } = {}) => {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,
+
         secure: false,
         auth: {
-            user: 'aabdalnaby073@gmail.com',
-            pass: 'wkwuafmiaoluhoyg',
         },
     });
 
     const info = await transporter.sendMail({
-        from: '"No Reply" <aabdalnaby073@gmail.com>',
         to: to ? to : '',
         subject,
         text: textMessage,
