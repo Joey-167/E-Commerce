07c3b169fba8e189fc1692ccb9d9cb73e88fa9a3
Youssef E. Saber <emadyoussef247@gmail.com>
2024-09-11 21:01:07 +0300

    Delete Assignment_3 directory

--- Assignment_3/src/Middlewares/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./error-handling.middleware.js";
-export * from "./finders.middleware.js";
-export * from "./multer.middleware.js";
-export * from "./auth.middleware.js";
-export * from "./validation.middleware.js"
--- Assignment_3/src/Utils/index.js
+++
@@ -1,5 +0,0 @@
-export * from "./file-extenstions.utils.js";
-export * from "./error-class.utils.js";
-export * from "./cloudinary.utils.js";
-export * from "./enums.utils.js";
-export * from "./calculate-price.utils.js"
